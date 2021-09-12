fffd21f0c54fb7caeb98ed50ca3ca177eb53afc4
Josue <benjamin.soleado@gmail.com>
2021-09-12 16:19:42 -0600

    primer_commit

---
+++ src/paquete/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author Juárez Arevalo
+ */
+public class main {
+    public static void main(String[] args) {
+       
+        // TODO code application logic here
+        frm_calculadora frm = new frm_calculadora();
+        frm.show();
+    }
+}
